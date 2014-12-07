639914b43d6735132a9b335c076d33d4d8152e54
Reshma <reshma.alee@gmail.com>
2014-12-07 21:04:56 +0545

    tes

---
+++ webroot/subpages/company_pre_screen_question.php
@@ -0,0 +1,117 @@
+<div class="portlet box purple ">
+						<div class="portlet-title">
+							<div class="caption">
+								Driver Pre-Screen Questions
+							</div>
+						</div>
+						<div class="portlet-body form">
+								<div class="form-body">
+									<div class="form-group col-md-6">
+										<label class="col-md-6 control-label">Recruiter's Name : </label>
+										<div class="col-md-6">
+											<input type="text" class="form-control">
+										</div>
+									</div>
+									<div class="form-group col-md-6">
+										<label class="col-md-6 control-label">Applicant's Phone Number : </label>
+										<div class="col-md-6">
+											<input type="text" class="form-control">
+										</div>
+									</div>
+                                    <div class="form-group col-md-6">
+										<label class="col-md-6 control-label">Applicant's Name : </label>
+										<div class="col-md-6">
+											<input type="text" class="form-control">
+										</div>
+									</div>
+                                    <div class="form-group col-md-6">
+										<label class="col-md-6 control-label">Applicant's Email : </label>
+										<div class="col-md-6">
+											<input type="text" class="form-control">
+										</div>
+									</div>
+                                    <div class="form-group col-md-6">
+										<label class="col-md-6 control-label">Date : </label>
+										<div class="col-md-6">
+											<input type="text" class="form-control" placeholder="(MM/DD/YYYY)">
+										</div>
+									</div>
+                                    <div class="form-group col-md-6">
+										<label class="col-md-6 control-label">Position : </label>
+										<div class="col-md-6">
+											<input type="text" class="form-control" placeholder="S,T">
+										</div>
+									</div>
+                                    <div class="clearfix"></div>
+								</div>
+						</div>
+					</div>
+                    <div class="clearfix"></div>
+                    
+                    
+                        <div class="table-scrollable">
+                        <table class="table table-striped table-hover">
+                                    <thead>
+                                    <tr>
+										<th class="center">Screen Questions</th>
+                            
+										<th class="center">Answers</th>
+                                    </tr>
+                                    </thead>
+                                    <thead>
+                                        <tr>
+                                            <th class="center col-md-12">Intro</th>
+                                        </tr>
+                                    </thead>
+                                    <tr><td class="col-md-5">What made you decide to call Challenger today?</td><td class="col-md-7"><textarea class="form-control"></textarea></tr>
+                                    <tr><td class="col-md-5">What type of job are you interested in?</td><td class="col-md-7"><textarea class="form-control"></textarea></td></tr>
+                                    <tr><td class="col-md-5">How did you hear about this opportunity?(make sure we confirm which site on the internet they saw our posting)</td>
+                                        <td class="col-md-7"><textarea class="form-control"></textarea></td></tr>
+                                    <thead>
+                                        <tr>
+                                            <th class="center col-md-12">Basic Requirements</th>
+                                        </tr>
+                                    </thead>
+                                    <tr>
+                                        <td class="col-md-5">Are you legally eligible to work in Canada?</td><td><input type="radio" class="form-control" />Yes</td><td><input type="radio" class="form-control" />No</td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="col-md-5">Do you currently hold a valid Canadian passport?</td><td class="col-md-3"><input type="radio" class="form-control" />Yes</td><td class="col-md-4"><input type="radio" class="form-control" />No</td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="col-md-5">(If they do not have a valid Canadian passport)Do you have a Permanent Residency card and US Visa?</td><td class="col-md-3"><input type="radio" class="form-control" />Yes</td><td class="col-md-4"><input type="radio" class="form-control" />No</td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="col-md-5">Do you have a FAST card?</td><td class="col-md-3"><input type="radio" class="form-control" />Yes</td><td class="col-md-4"><input type="radio" class="form-control" />No</td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="col-md-5">Have you ever been convicted of a criminal offence for which a pardon has not been granted or, which could cause you to not cross the border?</td><td class="col-md-3"><input type="radio" class="form-control" />Yes</td><td class="col-md-4"><input type="radio" class="form-control" />No</td>
+                                    </tr>
+                                    
+                                    <tr><td class="col-md-5">Where do you live?</td><td class="col-md-7"><textarea class="form-control"></textarea></tr>
+                                    
+                                    <tr><td class="col-md-5">How do you feel about running team? (Do they have a partner?)</td><td class="col-md-7"><textarea class="form-control"></textarea></tr>
+                                    
+                                    <thead>
+                                        <tr>
+                                            <th class="center col-md-12">Discovery</th>
+                                        </tr>
+                                    </thead>
+                                    <tr>
+                                        <td class="col-md-5">When did you get your AZ License and have you been commercially driving consistently since you got your license?</td><td><input type="text" class="form-control" placeholder="Month" /></td><td><input type="text" class="form-control" placeholder="Year" /></td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="col-md-5">Are you currently driving for another carrier?If yes, who and for how long?</td><td><textarea class="form-control"></textarea></td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="col-md-5">Tell me about the work you are doing?</td><td class="col-md-3"><input type="radio" class="form-control" />Yes</td><td class="col-md-4"><input type="radio" class="form-control" />No</td>
+                                    </tr>                                    
+                            </table>
+                            <div class="clearfix"></div>
+                        </div>

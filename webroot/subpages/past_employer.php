fe6d747e8a82ab39d0bf53a5cef7c6dd293c54a1
Reshma <reshma.alee@gmail.com>
2014-12-08 20:22:14 +0545

    test

---
+++ webroot/subpages/past_employer.php
@@ -0,0 +1,20 @@
+<?php
+echo '<div id="toremove"><div class="table-scrollable">
+    <table class="table table-striped">
+                <tr><th>Past Employer</th></tr>
+                <tr><td colspan="2">Company Name<input type="text" class="form-control" /></td></tr>
+                <tr><td colspan="2">Address<input type="text" class="form-control" /></td></tr>
+                <tr><td>Supervisors Name:<input type="text" class="form-control"/></td><td>Phone #:<input type="text" class="form-control"/></td></tr>
+                <tr><td>Supervisors Email:<input type="text" class="form-control"/></td><td>Secondary Email:<input type="text" class="form-control"/></td></tr>
+                <tr><td>Employment Start Date:<input type="text" class="form-control"/></td><td>Employment End Date:<input type="text" class="form-control"/></td></tr>
+                <tr><td>Claims with this Employer:&nbsp;&nbsp;<input type="radio"/>&nbsp;&nbsp;Yes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio"/>&nbsp;&nbsp;&nbsp;&nbsp;No</td><td>Date Claims Occured:<input type="text" class="form-control"/></td></tr>
+                <tr><td colspan="2">Employment history confirmed by (Verifier Use Only):<input type="text" class="form-control"/></td></tr>
+                <tr><td>Signature:<input type="text" class="form-control"/></td><td>Date/Time:<input type="text" class="form-control" /></td></tr>  
+    </table>
+</div>
+<div class="delete">
+    <a href="javascript:void(0);" class="btn red" id="delete">Delete</a>
+</div>
+  </div>  
+';
+?> 

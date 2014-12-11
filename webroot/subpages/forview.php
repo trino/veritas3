d5b8660f0f1e191b477842c4a66ebf085a05b6a9
Anwar <justdoit2045@gmail.com>
2014-12-11 23:55:40 +0545

    test

--- webroot/subpages/forview.php
+++ webroot/subpages/forview.php
@@ -1,16 +1,16 @@
-                                        <div class="">
+                                        <div class="content bordertop borderbot col-md-5">
 												<h3 class="block">Document information</h3>
 												<div class="form-group">
 													<label class="control-label col-md-3 alignleft"><strong>Document type</strong>
 													</label>
-													<label class="control-label col-md-4 alignleft">
+													<label class="control-label col-md-8 alignleft">
 														Orders
 													</label>
 												</div>
 												<div class="form-group">
 													<label class="control-label col-md-3 alignleft"><strong>Uploaded by</strong>
 													</label>
-													<label class="control-label col-md-4 alignleft">
+													<label class="control-label col-md-8 alignleft">
 														Admin
 														
 													</label>
@@ -18,7 +18,7 @@
 												<div class="form-group">
 													<label class="control-label col-md-3 alignleft"><strong>Uploaded on</strong>
 													</label>
-													<label class="control-label col-md-4 alignleft">
+													<label class="control-label col-md-8 alignleft">
 														<?php echo date('Y-m-d H:i:s')?>
 														
 													</label>
@@ -26,23 +26,23 @@
 												<div class="form-group">
 													<label class="control-label col-md-3 alignleft"><strong>Description</strong>
 													</label>
-													<label class="control-label col-md-4 alignleft">
+													<label class="control-label col-md-8 alignleft">
 													Sample description goes here
 														
 													</label>
 												</div>
 											</div>
-											<div class="">
+											<div class="content bordertop borderbot col-md-5">
 												<h3 class="block">Atttachments</h3>
 												<div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 1</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 1</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 2</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 2</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
@@ -50,38 +50,39 @@
 												</div>
 												
 											</div>
-											<div class="">
+                                            <div class="clean"></div>
+											<div class="content bordertop borderbot col-md-5">
 												<h3 class="block">Sub-documents</h3>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Company pre-screen questions</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Company pre-screen questions</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Driver application</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Driver application</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Driver evaluation form</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Driver evaluation form</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Employment verification form</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Employment verification form</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Consent form</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Consent form</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
@@ -89,48 +90,51 @@
 												</div>
 												
 											</div>
-											<div class="">
+											<div class="content bordertop borderbot col-md-5">
 												<h3 class="block">Finalization</h3>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 1</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 1</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 2</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 2</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 3</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 3</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 4</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 4</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
                                                 <div class="form-group">
-													<label class="control-label col-md-3 alignleft"><strong>Document 5</strong>
+													<label class="control-label col-md-7 alignleft"><i class="icon-notebook"> &nbsp; </i><strong>Document 5</strong>
 													</label>
 													<label class="control-label col-md-4 alignleft">
 														&#x2713;
 													</label>
 												</div>
-												<div class="form-group">
+												
+												
+												
+											</div>
+                                            <div class="clean"></div>
+                                            <div class="form-group col-md-6">
                                                     <div class="col-sm-12">
                                                     <a href="#" class="btn btn-primary">Print Report</a>
                                                     </div>
                                                 </div>
-												
-												
-											</div>
+                                             <div class="clean"></div>   

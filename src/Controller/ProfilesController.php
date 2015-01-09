84059b442b6c43f0fdbde99df5c806ff0d32283f
Reshma <reshma.alee@gmail.com>
2015-01-09 21:38:05 +0545

    test

--- src/Controller/ProfilesController.php
+++ src/Controller/ProfilesController.php
@@ -663,14 +663,16 @@ class ProfilesController extends AppController {
         die();
    }
    
-   function getSuper($id = null)
+   function getProfileType($id = null)
    {
         $q = TableRegistry::get('Profiles');
-        $query = $q->find();
-        $que = $query->select('super')->where(['id'=>$id]);
-        $this->response->body($que);
+        $que = $q->find();
+        $que->select(['profile_type'])->where(['id'=>$id]);
+        $query = $que->first();
+        $this->response->body($query);
         return $this->response;
         die();
    }
+  
 }
 ?>
--- src/Template/Profiles/index.ctp
+++ src/Template/Profiles/index.ctp
@@ -3,7 +3,7 @@
 </style>
 
 <?php $settings = $this->requestAction('settings/get_settings'); ?>
-<?php $sidebar =$this->requestAction("settings/all_settings/".$this->Session->read('Profile.id')."/sidebar");?>
+<?php $sidebar =$this->requestAction("settings/all_settings/".$this->request->session()->read('Profile.id')."/sidebar");?>
 <h3 class="page-title">
     <?php echo ucfirst($settings->profile); ?>
 </h3>

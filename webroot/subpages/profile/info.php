9f75f295bac02fe30602a2bcb87a6c2367e7042a
Reshma <reshma.alee@gmail.com>
2015-01-26 17:28:42 +0545

    test

--- webroot/subpages/profile/info.php
+++ webroot/subpages/profile/info.php
@@ -144,7 +144,6 @@
                         <div class="form-group">
                             <label class="control-label">Driver Type</label>
                             <select name="driver" class="form-control select_driver req_driver">
-\
                                 <option value="">Select Driver Type</option>
                                 <option
                                     value="1" <?php if (isset($p) && $p->driver == 1) echo "selected='selected'"; ?>

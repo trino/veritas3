d7f2b438f35c54e3ee3b2aeeed7426b6f598c0cc
Van <info@trinoweb.com>
2015-02-09 12:02:09 -0500

    1
    2

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -98,7 +98,7 @@ class ClientsController extends AppController {
 	public function index() {
 	   if(isset($_GET['flash']))
        {
-                $this->Flash->success('You have to select a client');
+                $this->Flash->success('Please select a client to upload.');
        }
 	   $setting = $this->Settings->get_permission($this->request->session()->read('Profile.id'));
         if($setting->client_list==0)
